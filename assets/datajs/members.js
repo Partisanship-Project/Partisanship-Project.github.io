ff5b7971141e14c5c424b9aac8d2b2682bc71ddf
jsradford <jasonscottradford@gmail.com>
2018-10-13 15:51:08 -0400

    testing cors error fix 2

--- assets/datajs/members.js
+++ assets/datajs/members.js
@@ -1,7 +1,7 @@
 
 
 function getMetadata(name){
-    d3.csv("/assets/data/metadata.csv", function(data) {
+    d3.csv("partisanshipproject.github.io/assets/data/metadata.csv", function(data) {
         for (i=0; i<data.length; i++){
             if (data[i]==name){
                 return data[i]; //returns the row of data as {city: "seattle", state: "WA", population: 652405, land area: 83.9}
@@ -13,7 +13,7 @@ function getMetadata(name){
 //generates a list of members - ["John Kennedy", "John McCain"]
 function getMembers(){
     var members=[];
-    d3.csv("/assets/data/metadata.csv", function(data) {
+    d3.csv("partisanshipproject.github.io/assets/data/metadata.csv", function(data) {
         for (i=0; i<data.length; i++){
             members.push(data[i]['Fullname']);
         }
