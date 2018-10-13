c7258e7338b5435314d0b0ad933661963d338dc3
jsradford <jasonscottradford@gmail.com>
2018-10-13 15:47:27 -0400

    testing cors error fix

---
+++ assets/datajs/members.js
@@ -0,0 +1,22 @@
+
+
+function getMetadata(name){
+    d3.csv("/assets/data/metadata.csv", function(data) {
+        for (i=0; i<data.length; i++){
+            if (data[i]==name){
+                return data[i]; //returns the row of data as {city: "seattle", state: "WA", population: 652405, land area: 83.9}
+            }
+        }
+    });
+}
+
+//generates a list of members - ["John Kennedy", "John McCain"]
+function getMembers(){
+    var members=[];
+    d3.csv("/assets/data/metadata.csv", function(data) {
+        for (i=0; i<data.length; i++){
+            members.push(data[i]['Fullname']);
+        }
+    });
+    return members
+}
