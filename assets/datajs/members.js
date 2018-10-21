33029e743ff6281086daf2631c7243025451d9a9
jsradford <jasonscottradford@gmail.com>
2018-10-21 16:34:32 -0400

    more fixes

--- assets/datajs/members.js
+++ assets/datajs/members.js
@@ -7,28 +7,43 @@ function fetchMemberData(){
     }else{
         console.log('got here1')
         newdata=fetchMetaData(member_input);
-        $('official_name').text(newdata[0]['Fullname']);
+        //$('official_name').text(newdata[0].Fullname);
     }
 }
 
 function fetchMetaData(name){
-    var output=[];
+    var title=''; //[Chamber] [Full Name]
+    var affiliation = ''; //[party] from [State]
+    var party ='';
+    var state= '';
     d3.csv("../assets/data/metadata.csv", function(data) {
-        var row = data;
-        console.log(row)
-        if (row['Fullname']==name){
-            console.log('got here2')
-            output.push(row) 
+        console.log(data)
+        if (data.Fullname==name){
+            if (data.District==0){
+                title='Senator ';
+            } else {
+                title="Representative ";
+            }
+            if (data.Party=='D'){
+                party='Democratic Candidate for ';
+            }else if (data.Party=='R'){
+                party='Republican Candidate for ';
+            }else{
+                party='Candidate for ';
+            }
+            //output.push(data)
+            $('#official_name').text(title.concat(data.Fullname));
+            $('#affiliation').text(party.concat(data.State));
         }
     });
-    return output
+    //return output
 }
 
 //generates a list of members - ["John Kennedy", "John McCain"]
 function getMembers(){
     var members=[];
     d3.csv("../assets/data/metadata.csv", function(data) {
-        members.push(data['Fullname'])
+        members.push(data.Fullname)
     });
     return members
 }
