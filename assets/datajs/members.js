948e1cc43f561c19c58f25d0c2b37c815e361faf
jsradford <jasonscottradford@gmail.com>
2018-10-21 16:11:18 -0400

    more data fixes

--- assets/datajs/members.js
+++ assets/datajs/members.js
@@ -1,25 +1,27 @@
 
 function fetchMemberData(){
+    var newdata=[];
     var member_input=$("#member_select").val();
     if (member_input=='' || member_input=='Select a Member'){
         member_input=$("#member_select").val("Select a Member");
     }else{
         console.log('got here1')
-        var r=fetchMetaData(member_input);
-        $('official_name').text(r['Fullname']);
-        console.log(r);
+        newdata=fetchMetaData(member_input);
+        $('official_name').text(newdata[0]['Fullname']);
     }
-    
-    
 }
+
 function fetchMetaData(name){
-d3.csv("../assets/data/metadata.csv", function(data) {
-    var row = data;
-    console.log(row)
-    if (row['Fullname']==name){
-        return row
-    }
-});
+    var output=[];
+    d3.csv("../assets/data/metadata.csv", function(data) {
+        var row = data;
+        console.log(row)
+        if (row['Fullname']==name){
+            console.log('got here2')
+            output.push(row) 
+        }
+    });
+    return output
 }
 
 //generates a list of members - ["John Kennedy", "John McCain"]
